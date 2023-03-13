bf827d07516b51d35f1f3dddfc9ac397a6bc0534
venkatmedida <62527000+venkatmedida@users.noreply.github.com>
2023-03-13 15:15:34 -0500

    Update streamlit_app.py
    added breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Idly')
+streamlit.text('Pesarattu Upma')
+strealit.txt('Dosa')
+
