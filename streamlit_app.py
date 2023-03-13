eff752a03a85822e9079a7ae5197c9767405acba
venkatmedida <62527000+venkatmedida@users.noreply.github.com>
2023-03-13 15:37:21 -0500

    Update streamlit_app.py
    added default fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("pick some fruits", list(my_fruit_list.index))
+streamlit.multiselect("pick some fruits", list(my_fruit_list.index), ['Avacado', 'Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
 
