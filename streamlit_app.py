3b96a79f663c89e16c34868fa2a837d9059767cb
venkatmedida <62527000+venkatmedida@users.noreply.github.com>
2023-03-13 15:34:41 -0500

    Update streamlit_app.py
    updated index

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ streamlit.text('🥑 Avacado toast')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 streamlit.multiselect("pick some fruits", list(my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
