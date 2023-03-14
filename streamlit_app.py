f6374c44ea71bb49b09f360d8d1e83f033059f37
venkatmedida <62527000+venkatmedida@users.noreply.github.com>
2023-03-14 12:10:14 -0500

    added exception handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,16 +24,17 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-
-
-# create a dataframe with pandas using json_normalize api
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display the dataframe on streamlit
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to display information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+ 
+  except URLerror as e:
+    streamlit.error()
 
 streamlit.stop()
 
